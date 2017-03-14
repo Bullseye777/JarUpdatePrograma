009fad4af21bb57afbea7932c005c9189fabee02
Bullseye777 <aurimux777@gmail.com>
2017-03-14 19:26:09 +0200

    Atvaizdavimas

--- src/com/company/atvaizdavimas/Atvaizdavimas.java
+++ src/com/company/atvaizdavimas/Atvaizdavimas.java
@@ -8,12 +8,12 @@ import java.util.Scanner;
  * Created by A on 2017.03.14.
  */
 public class Atvaizdavimas {
-    private  Connection connection;
+    private Connection connection;
 
 
     public Atvaizdavimas() {
         try {
-             connection = DriverManager.getConnection(
+            connection = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/kcs",
                     "root",
                     ""
@@ -24,7 +24,28 @@ public class Atvaizdavimas {
 
         }
     }
-    public void Pasisveikinimas(){
+
+    public void Pasisveikinimas() {
+        System.out.println("Sveiki, pasirinkite ka norite padaryti 'Students' lenteleje");
+    }
+
+    public void Paklausimas() {
+        System.out.println("Norint ivesti duomenis pasirinkti '1', norint trinti, pasirinkti '2'");
+    }
+
+    public void Pasirinkimas() {
+        Scanner sc = new Scanner(System.in);
+        int pasirinkimas = sc.nextInt();
+        switch (pasirinkimas){
+
+            case 1 :
+
+                break;
+            case 2 :
+
+                break;
+        }
+
 
     }
 
