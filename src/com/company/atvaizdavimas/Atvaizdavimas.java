1e72476864ec088e46d3c04de9aeb679462c6030
Bullseye777 <aurimux777@gmail.com>
2017-03-14 20:12:10 +0200

    Insertas

--- src/com/company/atvaizdavimas/Atvaizdavimas.java
+++ src/com/company/atvaizdavimas/Atvaizdavimas.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
  */
 public class Atvaizdavimas {
     private Connection connection;
-
+    Scanner sc = new Scanner(System.in);
 
     public Atvaizdavimas() {
         try {
@@ -34,18 +34,68 @@ public class Atvaizdavimas {
     }
 
     public void Pasirinkimas() {
-        Scanner sc = new Scanner(System.in);
         int pasirinkimas = sc.nextInt();
-        switch (pasirinkimas){
+        switch (pasirinkimas) {
 
-            case 1 :
-
+            case 1:
+                Insertas();
                 break;
-            case 2 :
-
+            case 2:
+                Deletinimas();
                 break;
         }
 
+    }
+
+    public void Insertas() {
+
+
+        try {
+
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO `students` (`name`, `surname`, `phone`, `email`) VALUES (?, ?, ?, ?)");
+            System.out.println("Ivesti Varda");
+            String name = sc.nextLine();
+            System.out.println("Ivesti Pavarde");
+            String surname = sc.nextLine();
+            System.out.println("Ivesti Telefona");
+            String phone = sc.nextLine();
+            System.out.println("Ivesti El.pasta");
+            String email = sc.nextLine();
+            statement.setString(1, name);
+            statement.setString(2, surname);
+            statement.setString(3, phone);
+            statement.setString(4, email);
+
+            statement.executeUpdate();
+
+
+        } catch (Exception klaida) {
+            System.out.println(klaida);
+        }
+
+    }
+
+    private void Deletinimas() {
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM `students`;");
+            while (resultSet.next()) {
+                System.out.print(resultSet.getInt("id"));
+                System.out.print("|");
+                System.out.print(resultSet.getString("name"));
+                System.out.print("|");
+                System.out.print(resultSet.getString("surname"));
+                System.out.print("");
+                System.out.print(resultSet.getInt("phone"));
+                System.out.print("");
+                System.out.print(resultSet.getString("email"));
+                System.out.println("");
+
+
+            }
+        } catch (Exception klaida) {
+            System.out.println(klaida);
+        }
 
     }
 
